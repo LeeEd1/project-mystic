7ecdda4b58400d27b4b2a6bb29da98ca12370afe
LeeEd1 <>
2023-12-28 17:09:29 +0000

    removing hidden game board to do some tests

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,11 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const startGameButton = document.querySelector(".startgame");
     const newGameButton = document.querySelector(".newgame");
 
-    gameContainer.style.visibility = "hidden";
 
     startGameButton.addEventListener("click", function () {
         if (!gameStarted) {
-            gameContainer.style.visibility = "visible";
+
             startGame();
             gameStarted = true;
         }
