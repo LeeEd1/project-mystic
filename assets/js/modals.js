f5fa9d2144ca2f5b1e8a7018ceed0d4de56acf0c
LeeEd1 <>
2023-12-30 10:34:16 +0000

    Fully commented modals.js

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,13 +1,23 @@
+/**Opens modal
+ * sets diplay to block
+ */
 function openModal() {
     const modalContainer = document.querySelector(".modal-container");
     modalContainer.style.display = "block";
 }
 
+/**Closes modal
+ * Sets display to none
+*/
 function closeModal() {
     const modalContainer = document.querySelector(".modal-container");
     modalContainer.style.display = "none";
 }
 
+/**Shows the congratulations modal
+ * Shows time completed time in strong using template literal
+ * 
+ */
 function showCongratulations() {
     const congratulationsModal = document.getElementById("congratulations-modal");
     const matchTime = 60 - document.querySelector(".time-remaining").textContent;
@@ -20,13 +30,16 @@ function showCongratulations() {
 
 }
 
+/**Shows game over modal */
 function showGameOver() {
     const gameOverModal = document.getElementById("game-over-modal");
     gameOverModal.style.display = "block";
 }
 
+/**References open and close buttons */
 const openButton = document.querySelector(".open");
 const closeButton = document.querySelector(".close");
 
+/**Event listeners for clicks on modal */
 openButton.addEventListener("click", openModal);
 closeButton.addEventListener("click", closeModal);
